939566889bb26a59a478691a4f8db8b3a5feba5c
minwukim <minwu1221@gmail.com>
2025-03-06 16:23:40 +0400

    update file

--- train_qwen3b_math_grpo.py
+++ train_qwen3b_math_grpo.py
@@ -46,12 +46,12 @@ def correctness_reward_func(prompts, completions, answer, **kwargs) -> list[floa
     responses = [completion[0]['content'] for completion in completions]
     scores = [compute_score(r, a) for r, a in zip(responses, answer)]
 
-    # for i, (q, r, a, score) in enumerate(zip(prompts, responses, answer, scores)):
-    #     print('-'*20, f"Question {i+1}:\n{q[-1]['content']}", 
-    #           f"\nAnswer:\n{a}", 
-    #           f"\nResponse:\n{responses[i]}", 
-    #           f"\nExtracted:\n{r}", 
-    #           f"\nScore:\n{score}")
+    for i, (q, r, a, score) in enumerate(zip(prompts, responses, answer, scores)):
+        print('-'*20, f"Question {i+1}:\n{q[-1]['content']}", 
+              f"\nAnswer:\n{a}", 
+              f"\nResponse:\n{responses[i]}", 
+              f"\nExtracted:\n{r}", 
+              f"\nScore:\n{score}")
 
     return scores
 
@@ -75,12 +75,12 @@ training_args = GRPOConfig(
     output_dir = "qwen3b-math-grpo",
     bf16 = True,
     bf16_full_eval=True,
-    vllm_gpu_memory_utilization=0.6,
+    vllm_gpu_memory_utilization=0.4,
     max_prompt_length = max_prompt_length,
     max_completion_length = max_seq_length,
     run_name = "qwen3b-grpo-exp1",
     report_to = "wandb", 
-    do_eval=True,
+    # do_eval=True,
     per_device_train_batch_size=4,
     num_generations = 4,
     gradient_accumulation_steps = 4
