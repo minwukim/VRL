e60b8f7dd46d1501d590c763ea4bee29e85eb1d6
minwukim <minwu1221@gmail.com>
2025-03-13 22:43:53 +0400

    update

--- GRPO_custom.py
+++ GRPO_custom.py
@@ -135,6 +135,8 @@ class CustomGRPOTrainer(GRPOTrainer):
         self, inputs: dict[str, Union[torch.Tensor, Any]]
     ) -> dict[str, Union[torch.Tensor, Any]]:
         # 1. Preprocess the original prompt (Q)
+        device = self.accelerator.device
+        print(inputs)
         prompts = [x["prompt"] for x in inputs]
         prompts_text = [maybe_apply_chat_template(example, self.processing_class)["prompt"] for example in inputs]
 
