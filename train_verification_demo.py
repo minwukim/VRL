1437cfcdad804dbdf5ea9d23e0249d1b5729a19d
minwukim <minwu1221@gmail.com>
2025-03-23 23:33:43 +0400

    update train

--- train_verification_demo.py
+++ train_verification_demo.py
@@ -88,21 +88,23 @@ training_args = GRPOConfig(
     eval_steps = 200,
     log_completions = True
 )
-# trainer = VerificationGRPOTrainer(
-#     model=model_name,
-#     reward_funcs=[reward_correct, reward_correct_and_format],
-#     args = training_args,
-#     train_dataset=train,
-#     eval_dataset=test,
-# )
 
-trainer = SwitchingGRPOTrainer(
+
+trainer = VerificationGRPOTrainer(
     model=model_name,
     reward_funcs=[reward_correct, reward_correct_and_format],
     args = training_args,
     train_dataset=train,
     eval_dataset=test,
 )
+
+# trainer = SwitchingGRPOTrainer(
+#     model=model_name,
+#     reward_funcs=[reward_correct, reward_correct_and_format],
+#     args = training_args,
+#     train_dataset=train,
+#     eval_dataset=test,
+# )
 
 # trainer = ReplicatedGRPOTrainer(
 #     model=model_name,
