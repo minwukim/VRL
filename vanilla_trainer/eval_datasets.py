67ad5617f470ff1f8ec1ca013c4f6222e41d7b48
Safal Shrestha <safal.bp2@gmail.com>
2025-03-25 10:59:37 +0000

    more kk eval

--- vanilla_trainer/eval_datasets.py
+++ vanilla_trainer/eval_datasets.py
@@ -1,52 +1,53 @@
 from datasets import load_dataset
 import re
 
-def get_kk_test_prompts():
 
-    def reward_kk(output, ground_truth):
-        def preprocess(text):
-           # pattern = r".+</think>\s*<answer>(.+)</answer>\s*$"
-           # match = re.search(pattern, text, re.DOTALL)
-           # if match: text = match.group(1)
+def reward_kk(output, ground_truth):
+    def preprocess(text):
+       # pattern = r".+</think>\s*<answer>(.+)</answer>\s*$"
+       # match = re.search(pattern, text, re.DOTALL)
+       # if match: text = match.group(1)
 
-            lines = text.split("\n")
-            solution = {}
+        lines = text.split("\n")
+        solution = {}
+        
+        for line in lines:
+            sp = line.split(" ")
+            if not line: continue
+            if len(sp) < 2: continue
 
-            for line in lines:
-                sp = line.split(" ")
-                if not line: continue
-                if len(sp) < 2: continue
-
-                solution[sp[1].lower()] = sp[-1].lower()
-            return solution
+            solution[sp[1].lower()] = sp[-1].lower()
+        return solution
 
          
-        pattern = r".+</think>\s*<answer>(.+)</answer>\s*$"
-        s = output 
-        if not (s.count("</think>") == 1 and s.count("<answer>") == 1 and s.count("</answer>") == 1):
-            # incorrect amount of tokens
-            return -2 
-        match = re.search(pattern, s, re.DOTALL)
-        # if answer doesn't match provided format
-        if not match: return -2
+    pattern = r".+</think>\s*<answer>(.+)</answer>\s*$"
+    s = output 
+    if not (s.count("</think>") == 1 and s.count("<answer>") == 1 and s.count("</answer>") == 1):
+        # incorrect amount of tokens
+        return -2 
+    match = re.search(pattern, s, re.DOTALL)
+    # if answer doesn't match provided format
+    if not match: return -2
 
-        # answer format is correct now
-        # 
-        
-        gt = preprocess(ground_truth)
-        out = preprocess(match.group(1))
-        print(out)
-        names = out.keys()
-        for name in names:
-            val = gt.get(name, None)
-            if val is None: return -1
+    # answer format is correct now
+    # 
+    
+    gt = preprocess(ground_truth)
+    out = preprocess(match.group(1).strip())
+    print(out)
+    names = out.keys()
+    for name in names:
+        val = gt.get(name, None)
+        if val is None: return -1
 
-        roles = out.values()
-        for role in roles:
-            if role.lower() != 'knight' and role.lower() != 'knave': return -1
+    roles = out.values()
+    for role in roles:
+        if role.lower() != 'knight' and role.lower() != 'knave': return -1
 
-        # formatting of final answer also good
-        return 2 if gt == out else -0.5
+    # formatting of final answer also good
+    return 2 if gt == out else -0.5
+def get_kk_test_prompts():
+
 
     SYSTEM_PROMPT = """
     A conversation between User and Assistant. The user asks a question, and the Assistant solves it.
@@ -70,16 +71,3 @@ def get_kk_test_prompts():
 
     return prompts, ground_truths, reward_kk
 
-#prompts, ground_truth, reward_kk = get_kk_test_prompts()
-#
-##print(reward_kk(prompts[0], ground_truth[0]))
-#
-#out_string = """
-#Hello there </think><answer>
-#(1) Minwu is a knave
-#(2) Safal is a knight
-#(3)</answer>
-#"""
-#
-#ground_truth = "(1) Safal is a knight\n(2) Minwu is a knave"
-#print(reward_kk(out_string, ground_truth))
