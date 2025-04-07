acb107ba293f4eab66663ce0803ee7feaf4a6f8c
minwukim <minwu1221@gmail.com>
2025-04-07 22:07:50 +0400

    update

--- evaluation_code/vllm_test_1turn_and_confidence.py
+++ evaluation_code/vllm_test_1turn_and_confidence.py
@@ -9,8 +9,8 @@ from math_verify import verify, parse
 # model_path = "Qwen/Qwen2.5-Math-1.5B-Instruct"
 # csv_path = "qwen_1.5b_math_it_base.csv"
 
-model_path = "../qwen2.5-1.5B-MATH-it-vanilla-GRPO/checkpoint-500"
-csv_path = "qwen_1.5b_math_it_cp500.csv"
+model_path = "../qwen2.5-1.5B-MATH-it-vanilla-GRPO-scaleTrue/checkpoint-100"
+csv_path = "qwen_1.5b_math_it_vanilla_scaleTrue_cp100.csv"
 
 
 
