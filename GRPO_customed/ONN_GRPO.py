734694928601dfadbdeb811a771619538b55f7b2
minwukim <minwu1221@gmail.com>
2025-04-15 15:37:45 +0400

    update

--- GRPO_customed/ONN_GRPO.py
+++ GRPO_customed/ONN_GRPO.py
@@ -174,7 +174,7 @@ class ONN_GRPOTrainer(GRPOTrainer):
     def __init__(
         self,
         *args,
-        correction_instruction: str = "\nUser: There might be an error in the solution above because of lack of understanding of the question. Please correct the error, if any, and rewrite the solution. Maintain the format of: <think> reasoning process here </think> <answer> \\boxed{{final answer inside}} </answer>.\nAssistant: <think>",
+        correction_instruction: str = "<|im_end|>\n <|im_start|>user\n There might be an error in the solution above because of lack of understanding of the question. Please correct the error, if any, and rewrite the solution. Maintain the format of: <think> reasoning process here </think> <answer> \\boxed{{final answer inside}} </answer>.<|im_end|>\n<|im_start|>assistant\n <think>",
         # correction_instruction: str = "There might be something wrong with the previous response. Please review it critically and provide a corrected or improved answer. If the previous response was already correct, reiterate it.",
         a1_prompt_format: str = "{prompt}{completion}{instruction}",
         # a1_prompt_format: str = "{prompt}{completion}<|im_end|>\n<|im_start|>user\n{instruction}<|im_end|>\n<|im_start|>assistant\n",
