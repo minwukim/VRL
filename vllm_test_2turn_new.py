1619c5d2b31f40a60ef5c8b53f0d59fa738ad56f
minwukim <minwu1221@gmail.com>
2025-04-16 11:38:05 +0400

    ud

--- vllm_test_2turn_new.py
+++ vllm_test_2turn_new.py
@@ -9,7 +9,7 @@ from math_verify import verify, parse
 # Settings and Model Initialization
 ##############################################
 
-model_path = "./0415-qwen3b-it-ONN/checkpoint-300"
+model_path = "./0415-qwen3b-it-ONN/checkpoint-150"
 # csv_file_path = "0415-qwen3b-it-ONN-a1agnostic-cp150.csv"
 
 # First turn prompt template
