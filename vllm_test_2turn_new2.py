128d30a600f383176e56dccde868d9244109d97b
minwukim <minwu1221@gmail.com>
2025-04-21 11:27:02 +0400

    ud

--- vllm_test_2turn_new2.py
+++ vllm_test_2turn_new2.py
@@ -104,7 +104,7 @@ def get_math_test_data():
 
 # model_path = "./0417-qwen3b-it-OON-oracle-switch/checkpoint-200"
 # model_path = "./qwen3b-it-old-prompt/checkpoint-350"
-model_path = "./qwen3b-it-SFT-boxed/checkpoint-175"
+model_path = "./qwen3b-it-SFT-boxed/checkpoint-300"
 # model_path = "Qwen/Qwen2.5-3B"
 llm = LLM(model=model_path)
 
