5edb7e2da5e943303e86a24ea3b250289cad9d65
minwukim <minwu1221@gmail.com>
2025-04-23 01:49:04 +0400

    ud

--- vllm_256_collection.py
+++ vllm_256_collection.py
@@ -11,8 +11,8 @@ from pathlib import Path
 # Config
 # ——————————————
 model_path = "Qwen/Qwen2.5-3B"  # Path to the model
-csv_path = "demo5.csv"
-seed = 5
+csv_path = "demo7.csv"
+seed = 7
 
 num_trials = 256
 batch_size = 100000  # Adjust based on memory capacity
