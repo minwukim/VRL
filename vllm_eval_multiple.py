f75c3cc1132c909b1d9564133f3857b1debefac0
minwukim <minwu1221@gmail.com>
2025-04-25 11:34:35 +0400

    us

--- vllm_eval_multiple.py
+++ vllm_eval_multiple.py
@@ -13,7 +13,8 @@ from math_verify import verify, parse
 # model_path = "./qwen3b-it-SFT-boxed/checkpoint-250"
 
 # model_path = "./0422-qwen3b-question-only-no-format-weighted-sft-cp175/checkpoint-125"
-model_path = "./vanilla_trainer/0425-base-self-distill/checkpoint-41718"
+# model_path = "./vanilla_trainer/0425-base-self-distill/checkpoint-41718"
+model_path = "./vanilla_trainer/0425-cp150-self-distill/checkpoint-41730"
 
 # csv_path = "0421-qwen3b-question-only-no-format-online-sft-cp50.csv"
 
