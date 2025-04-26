0dcf0f76d8dc1712715d693ce97822e443fd475f
minwukim <minwu1221@gmail.com>
2025-04-27 02:42:35 +0400

    ud

--- vllm_data_collection.sh
+++ vllm_data_collection.sh
@@ -2,7 +2,7 @@
 for gpu in $(seq 0 9); do
   CUDA_VISIBLE_DEVICES=$gpu \
   nohup python vllm_data_collection.py --gpu-id $gpu \
-        > cp200_log_gpu${gpu}.txt 2>&1 &
+        > cp100_log_gpu${gpu}_new.txt 2>&1 &
 done
 wait
 # -------------------------------------------------------------------
