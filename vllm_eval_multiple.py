aa871477b7cb0d82c92caf63a1e4ef384002bd05
minwukim <minwu1221@gmail.com>
2025-05-02 02:39:01 +0400

    ud

--- vllm_eval_multiple.py
+++ vllm_eval_multiple.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from datasets import load_dataset
 from vllm import LLM, SamplingParams
-from obsolete.custom_MATH_reward import remove_boxed, last_boxed_only_string
+# from obsolete.custom_MATH_reward import remove_boxed, last_boxed_only_string
 from math_verify import verify, parse
 
 # ——————————————
@@ -54,6 +54,35 @@ SYSTEM_PROMPT_6 = (
 
 
 SYSTEM_PROMPT = SYSTEM_PROMPT_6
+
+def last_boxed_only_string(string):
+    idx = string.rfind("\\boxed")
+    if "\\boxed " in string:
+        return "\\boxed " + string.split("\\boxed ")[-1].split("$")[0]
+    if idx < 0:
+        idx = string.rfind("\\fbox")
+        if idx < 0:
+            return None
+
+    i = idx
+    right_brace_idx = None
+    num_left_braces_open = 0
+    while i < len(string):
+        if string[i] == "{":
+            num_left_braces_open += 1
+        if string[i] == "}":
+            num_left_braces_open -= 1
+            if num_left_braces_open == 0:
+                right_brace_idx = i
+                break
+        i += 1
+
+    if right_brace_idx is None:
+        retval = None
+    else:
+        retval = string[idx:right_brace_idx + 1]
+
+    return retval
 
 # ——————————————
 # Reward functions
