8b7695caade1d391be3dccc8a17de04168645f60
minwukim <minwu1221@gmail.com>
2025-05-09 21:16:30 +0400

    ud

--- vllm_ood_256.py
+++ vllm_ood_256.py
@@ -28,7 +28,7 @@ model_path = "./qwq_distill_cps/4-all-checkpoint/4-all-checkpoint"
 # csv_train_path = "1to64_kk_response.csv"
 # csv_train_path = "np128p256_kk.csv"
 # csv_train_path = "174_incorrect_response_second.csv"
-csv_train_path = "4all_first10.csv"
+csv_train_path = "4all_last246.csv"
 
 column_name = 'kk_not_solved'
 
@@ -41,8 +41,8 @@ column_name = 'kk_not_solved'
 
 
 # csv_test_path = "QwQ_test.csv"
-seed = 1
-num_trials = 10
+seed = 2
+num_trials = 246
 batch_size = 150000
 temperature = 0.9
 top_p = 1
@@ -172,6 +172,9 @@ def run_evaluation(csv_path, problems, ground_truths, question_indices, dataset_
 # ds_train = pd.read_csv("base_model_nopass128_pass256_76.csv")
 # ds_train = pd.read_csv("base_model_test_question_solution_hit.csv")
 ds_train = pd.read_csv("distilled_models_128_unsolved.csv")
+target_indices = [9, 11, 21, 43, 64, 80, 96, 100, 101, 103, 104, 110, 126, 138, 147, 154, 156, 166, 168, 184, 189, 222, 224, 232, 239, 240, 242, 264, 267, 274, 285, 286, 292, 295, 303, 306, 308, 324, 327, 351, 352, 365, 369, 381, 392, 400, 401, 403, 422, 425, 460, 466, 475, 478, 481, 494, 497]
+ds_train = ds_train[ds_train['question_index'].isin(target_indices)]
+
 # ds_train = ds_train[ds_train[column_name] == 1]
 
 train_problems = [SYSTEM_PROMPT.format(prompt=p) for p in ds_train['prompt']]
