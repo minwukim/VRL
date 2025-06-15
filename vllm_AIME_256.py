1e327c254c3e82631bbdd59ac680a314cb349b9b
minwukim <minwu1221@gmail.com>
2025-06-15 09:11:41 +0400

    ud

--- vllm_AIME_256.py
+++ vllm_AIME_256.py
@@ -62,7 +62,7 @@ top_p = 1
 top_k = 40
 min_p = 0.0
 presence_penalty = 1.0
-tensor_parallel_size = 1
+tensor_parallel_size = 2
 
 # Prompt template with standardized instruction
 # SYSTEM_PROMPT = (
