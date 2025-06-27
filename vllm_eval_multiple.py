ce289d11a2e71e1b36e80652182347c209b0dfe7
minwukim <minwu1221@gmail.com>
2025-06-27 23:50:01 +0400

    ud

--- vllm_eval_multiple.py
+++ vllm_eval_multiple.py
@@ -15,7 +15,7 @@ from math_verify import verify, parse
 # model_path ="Qwen/Qwen2.5-3B-instruct"
 # model_path = "Qwen/Qwen2.5-3B"
 # model_path = "Qwen/Qwen2.5-Math-1.5B"
-model_path = "./0627-3B-7500-24-gen/checkpoint-150"
+model_path = "./0627-3B-7500-24-gen/checkpoint-75"
 
 
 # FOLLOWING THE SOBER PAPERR
