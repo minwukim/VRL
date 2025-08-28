78e11a1d028a3f4e1da5f25aac7b4a847928b958
minwukim <minwu1221@gmail.com>
2025-08-28 15:02:58 +0400

    ud

--- confidence_tag/statcheck.py
+++ confidence_tag/statcheck.py
@@ -4,7 +4,9 @@ df_inter = pd.read_csv("aime25_results_vllm_interrupted_qwen3_32B_0827.csv")
 df_normal = pd.read_csv("aime25_results_vllm_normal_qwen3_32B_0827.csv")
 
 print("interrupted df shape: ", df_inter.shape)
-print("interrupted tokens: ", df_inter[:df_inter.shape[0]-df_inter.shape[0]%5]['token_count'].mean())
-print("normal tokens: ", df_normal[:df_inter.shape[0]-df_inter.shape[0]%5]['token_count'].mean())
-print("interrupted reward: ", df_inter[:df_inter.shape[0]-df_inter.shape[0]%5]['reward'].mean())
-print("normal reward: ", df_normal[:df_inter.shape[0]-df_inter.shape[0]%5]['reward'].mean())
+print("normal df shape: ", df_normal.shape)
+to_check = df_normal.shape[0]
+print("interrupted tokens: ", df_inter[:to_check]['token_count'].mean())
+print("normal tokens: ", df_normal[:to_check]['token_count'].mean())
+print("interrupted reward: ", df_inter[:to_check]['reward'].mean())
+print("normal reward: ", df_normal[:to_check]['reward'].mean())
