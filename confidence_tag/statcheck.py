e4d91504849a3b17b14a701bc297551d9ea42eee
minwukim <minwu1221@gmail.com>
2025-08-28 15:01:24 +0400

    ud

--- confidence_tag/statcheck.py
+++ confidence_tag/statcheck.py
@@ -5,6 +5,6 @@ df_normal = pd.read_csv("aime25_results_vllm_normal_qwen3_32B_0827.csv")
 
 print("interrupted df shape: ", df_inter.shape)
 print("interrupted tokens: ", df_inter[:df_inter.shape[0]-df_inter.shape[0]%5]['token_count'].mean())
-print("normal tokens: ", df_normal['token_count'].mean())
+print("normal tokens: ", df_normal[:df_inter.shape[0]-df_inter.shape[0]%5]['token_count'].mean())
 print("interrupted reward: ", df_inter[:df_inter.shape[0]-df_inter.shape[0]%5]['reward'].mean())
-print("normal reward: ", df_normal['reward'].mean())
+print("normal reward: ", df_normal[:df_inter.shape[0]-df_inter.shape[0]%5]['reward'].mean())
